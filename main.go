79b6f2047d5b405a6e73852c91cdccaa545aa11d
MakMemEng <yatani.mems4568@gmail.com>
2024-02-10 15:48:05 +0000

    コードの説明コメントを追記

--- main.go
+++ main.go
@@ -8,6 +8,20 @@ import (
 
 func main() {
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+	/* 	// io.Writer interface型の変数
+		var w io.Writer
+
+		// Write(p []byte) (n int, err, error)を持つ構造体
+		type MyType1 struct{}
+		func (t MyType1) Write(p []byte) (n int, err, error) {
+			//
+		}
+		w = MyType1{} // 代入OK
+
+		// Write(p []byte) (n int, err, error)を持たない構造体
+		type MyType2 struct{}
+		w = MyType2{} // 代入NG(コンパイルエラー)
+	 */
 		io.WriteString(w, "Hello, World!\n")
 	}
 
@@ -15,4 +29,8 @@ func main() {
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	/* 以下と同じ意味
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+	*/
 }
