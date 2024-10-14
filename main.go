9f9c3a73ae053f281138b9b1e187859736b68e13
MakMemEng <yatani.mems4568@gmail.com>
2024-10-14 08:02:52 +0000

    ルーティングの処理をnet/httpからgorilla/muxに置き換え

--- main.go
+++ main.go
@@ -5,22 +5,28 @@ import (
 	"net/http"
 
 	"github.com/MakMemEng/go-api-middle-road/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handlers.HelloHandler)
+	// ルータを明示的に宣言
+	r := mux.NewRouter()
 
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	// http.HandleFunc("/", handlers.HelloHandler)
+	// http.HandleFunc("/article", handlers.PostArticleHandler)
+	// http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	// http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	// http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	// http.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	/* 以下と同じ意味
-	err := http.ListenAndServe(":8080", nil)
-	log.Fatal(err)
-	*/
+	log.Fatal(http.ListenAndServe(":8080", r))
+	// log.Fatal(http.ListenAndServe(":8080", nil))
 }
